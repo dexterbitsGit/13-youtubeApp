7404397680c14fed4e58fbf3eb3dc1eca929642c
Jose Guillermo Perez <jperez.dextersoft@gmail.com>
2020-09-08 08:38:18 -0300

    Desarrollo de la logica del servicio para obtener la data de la API de Youtube

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class YoutubeService {
+  // https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyBczsC9nqZBQECiJYDXKM7aPvOUjEqbFww&playlistId=UU2ZEYYMMDt7mNOxKOzoQtqQ&maxResults=4
 
-  constructor() { }
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apikey = 'AIzaSyBczsC9nqZBQECiJYDXKM7aPvOUjEqbFww';
+  private playlist = 'UU2ZEYYMMDt7mNOxKOzoQtqQ';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) {}
+
+  getVideos() {
+    const url = ` ${this.youtubeUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '4')
+      .set('playlistId', this.playlist)
+      .set('key', this.apikey);
+
+    return this.http.get(url, { params });
+  }
 }
