4c7ff6afb138e6dcf1ed0938ab8f817fac6207bf
Jose Guillermo Perez <jperez.dextersoft@gmail.com>
2020-09-09 08:32:20 -0300

    desarrollo de lo logica de cards con los videos precargados, realizamos el tipado con el youtube models, creamos una ventana con los videos a reproducir

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { YoutubeResponse } from '../models/youtube.models';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class YoutubeService {
-  // https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyBczsC9nqZBQECiJYDXKM7aPvOUjEqbFww&playlistId=UU2ZEYYMMDt7mNOxKOzoQtqQ&maxResults=4
-
   private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
-  private apikey = 'AIzaSyBczsC9nqZBQECiJYDXKM7aPvOUjEqbFww';
-  private playlist = 'UU2ZEYYMMDt7mNOxKOzoQtqQ';
+  // private apikey = 'AIzaSyBczsC9nqZBQECiJYDXKM7aPvOUjEqbFww';
+  // private playlist = 'UU2ZEYYMMDt7mNOxKOzoQtqQ';
+  private apikey = 'AIzaSyBHLZzytcylRRpuFepi2mHyMD3Ede-r7-s';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
   private nextPageToken = '';
 
   constructor(private http: HttpClient) {}
@@ -18,10 +20,19 @@ export class YoutubeService {
     const url = ` ${this.youtubeUrl}/playlistItems`;
     const params = new HttpParams()
       .set('part', 'snippet')
-      .set('maxResults', '4')
+      .set('maxResults', '10')
       .set('playlistId', this.playlist)
       .set('key', this.apikey);
 
-    return this.http.get(url, { params });
+    return this.http
+      .get<YoutubeResponse>(url, { params })
+      .pipe(
+        map((resp) => {
+          this.nextPageToken = resp.nextPageToken;
+          return resp.items;
+        }),
+
+        map((items) => items.map((video) => video.snippet))
+      );
   }
 }
